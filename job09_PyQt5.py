9fcde3fe15d98b25945e99729571283be2ab4492
Hajinjing <97497349+Hajinjing@users.noreply.github.com>
2022-04-13 16:53:37 +0900

    pyqt5

--- job09_PyQt5.py
+++ job09_PyQt5.py
@@ -14,45 +14,45 @@ import numpy as np
 
 form_window = uic.loadUiType('./mainWidget.ui')[0]
 
-# class Exam(QWidget, form_window):
-#     def __init__(self):
-#         super().__init__()
-#         self.setupUi(self)
-#         self.df_reviews = pd.read_csv(
-#             './crawling_data/datasets/movie_review_2018_2022.csv')
-#         self.Tfidf_matrix = mmread('./models/Tfidf_movie_review01.mtx').tocsr()
-#         self.titles = list(self.df_reviews['title'])
-#         self.titles.sort()
-#         for title in self.titles:
-#             self.cmb_titles.addItem(title)
-#         self.cmb_titles.currentIndexChanged.connect(
-#             self.cmb_titles_slot)
+class Exam(QWidget, form_window):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.df_contents = pd.read_csv(
+            './refined_data/cleaned_data.csv')
+        self.Tfidf_matrix = mmread('./models/Tfidf_tour.mtx').tocsr()
+        self.titles = list(self.df_contents['title'])
+        self.titles.sort()
+        for title in self.titles:
+            self.cmb_titles.addItem(title)
+        self.cmb_titles.currentIndexChanged.connect(
+            self.cmb_titles_slot)
 #
-#     def cmb_titles_slot(self):
-#         title = self.cmb_titles.currentText()
-#         recommendation_titles = self.recommend_by_movie_title(title)
-#         self.lbl_recommend.setText(recommendation_titles)
+    def cmb_titles_slot(self):
+        title = self.cmb_titles.currentText()
+        recommendation_titles = self.recommend_by_tour_title(title)
+        self.lbl_recommend.setText(recommendation_titles)
+
+    def recommend_by_tour_title(self, title):
+        tour_idx = self.df_reviews[self.df_reviews['title']==title].index[0]
+        cosine_sim = linear_kernel(self.Tfidf_matrix[tour_idx],
+                                   self.Tfidf_matrix)
+        recommendation_titles = self.getRecommendation(cosine_sim)
+        recommendation_titles = '\n'.join(list(recommendation_titles))
+        return recommendation_titles
+
+
+    def getRecommendation(self, cosine_sim):
+        simScore = list(enumerate(cosine_sim[-1]))
+        simScore = sorted(simScore, key=lambda x: x[1],
+                          reverse=True)
+        simScore = simScore[1:11]
+        touridx = [i[0] for i in simScore]
+        recTourList = self.df_reviews.iloc[touridx]
+        return recTourList.iloc[:, 0]
 #
-#     def recommend_by_movie_title(self, title):
-#         movie_idx = self.df_reviews[self.df_reviews['title']==title].index[0]
-#         cosine_sim = linear_kernel(self.Tfidf_matrix[movie_idx],
-#                                    self.Tfidf_matrix)
-#         recommendation_titles = self.getRecommendation(cosine_sim)
-#         recommendation_titles = '\n'.join(list(recommendation_titles))
-#         return recommendation_titles
 #
-#
-#     def getRecommendation(self, cosine_sim):
-#         simScore = list(enumerate(cosine_sim[-1]))
-#         simScore = sorted(simScore, key=lambda x: x[1],
-#                           reverse=True)
-#         simScore = simScore[1:11]
-#         movieidx = [i[0] for i in simScore]
-#         recMovieList = self.df_reviews.iloc[movieidx]
-#         return recMovieList.iloc[:, 0]
-#
-#
-# app = QApplication(sys.argv)
-# mainWindow = Exam()
-# mainWindow.show()
-# sys.exit(app.exec_())
+app = QApplication(sys.argv)
+mainWindow = Exam()
+mainWindow.show()
+sys.exit(app.exec_())
