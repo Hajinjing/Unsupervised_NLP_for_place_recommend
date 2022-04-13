95db861ec5e572b225cc63faa3f3efac0236f7d5
Hajinjing <97497349+Hajinjing@users.noreply.github.com>
2022-04-13 11:57:19 +0900

    job08까지 commit

---
+++ job06_tfidf.py
@@ -0,0 +1,24 @@
+#TF-IDF : 단어의 빈도와 역 문서 빈도를 사용하여 DTM내의 각 단어들마다 중요한 정도를 가중치로 주는 방법, 문사의 유사도를 구하는 작업
+#단어 카운트 가중치
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.io import mmwrite, mmread
+import pickle
+
+df_contents = pd.read_csv('./refined_data/cleaned_data.csv')
+# df_contents.dropna(inplace=True)
+# # df_contents.to_csv('./refined_data/cleaned_data.csv',
+# #                   index=False)
+df_contents.info()
+
+Tfidf = TfidfVectorizer(sublinear_tf=True)
+# Tfidf_matrix = Tfidf.fit_transform(df_contents['cleaned_sentences'])
+#
+# with open('./models/tfidf01.pickle', 'wb') as f:
+#     pickle.dump(Tfidf, f)
+# mmwrite('./models/Tfidf_tour.mtx', Tfidf_matrix)
+# print('end')
+
+
+
+
---
+++ job07_recommendation.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn.metrics.pairwise import linear_kernel
+from scipy.io import mmread, mmwrite
+import pickle
+from gensim.models import Word2Vec
+
+df_contents = pd.read_csv('./refined_data/cleaned_data.csv')
+df_contents.info()
+def getRecommendation(cosine_sim):
+    simScore = list(enumerate(cosine_sim[-1]))
+    # print(len(simScore))
+    # print(simScore)
+    simScore = sorted(simScore, key=lambda x:x[1],
+                      reverse=True)
+    simScore = simScore[1:11]
+    touridx = [i[0] for i in simScore]
+    recTourList = df_contents.iloc[touridx]
+    return recTourList.iloc[:, 0]
+
+Tfidf_matrix = mmread('./models/Tfidf_tour.mtx').tocsr()
+with open('./models/tfidf01.pickle', 'rb') as f:
+    Tfidf = pickle.load(f)
+
+# # 영화 제목을 이용
+# tour_idx = df_contents[df_contents['title']=='서울대공원'].index[0]
+# print(tour_idx)
+# 영화 index를 이용
+# movie_idx = 566
+# print(df_reviews.iloc[movie_idx, 0])
+
+# key_word 이용
+embedding_model = Word2Vec.load('./models/word2vecModel.model')
+key_word = '데이트'
+sim_word = embedding_model.wv.most_similar(key_word, topn=10)
+sentence = [key_word] * 11
+words = []
+for word, _ in sim_word:
+    words.append(word)
+for i, word in enumerate(words):
+    sentence += [word] * (10 - i)
+#
+sentence = ' '.join(sentence)
+sentence = '데이트'
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+
+# cosine_sim = linear_kernel(Tfidf_matrix[tour_idx], Tfidf_matrix)
+recommendation = getRecommendation(cosine_sim)
+
+print(recommendation)
+
+
+
+
+
+
+
+
