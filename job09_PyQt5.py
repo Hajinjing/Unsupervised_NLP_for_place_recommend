055d6b73bb925a52d7bd042736956e802ca8aab4
Hajinjing <97497349+Hajinjing@users.noreply.github.com>
2022-04-14 15:41:13 +0900

    파일명수정 commit

--- job09_PyQt5.py
+++ job09_PyQt5.py
@@ -6,49 +6,60 @@ from gensim.models import Word2Vec
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtCore import QUrl
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QPixmap
 from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-form_window = uic.loadUiType('./mainWidget.ui')[0]
+form_window = uic.loadUiType('./webWidget.ui')[0]
+class WebView(QWebEngineView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def release(self):
+        self.deleteLater()
+        self.close()
 
 class Exam(QWidget, form_window):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.df_reviews = pd.read_csv(
-            './crawling_data/datasets/movie_review_2018_2022.csv')
-        self.Tfidf_matrix = mmread('./models/Tfidf_movie_review01.mtx').tocsr()
+        self._webview = WebView()
+        self.web.addWidget(self._webview)
+        # self._webview.load(QUrl('http://www.naver.com'))
+        self._webview.load(QUrl('http://localhost:63342/Unsupervised_NLP_for_place_recommend2/html/search_place_main.html?_ijt=8qjrtd7rv06g0t2osit3t9d8eh&_ij_reload=RELOAD_ON_SAVE'))
+        self.df_contents = pd.read_csv('./refined_data/cleaned_data.csv')
+        self.Tfidf_matrix = mmread('./models/Tfidf_tour.mtx').tocsr()
         with open('./models/tfidf01.pickle', 'rb') as f:
             self.Tfidf = pickle.load(f)
-        self.titles = list(self.df_reviews['title'])
-        self.titles.sort()
-        for title in self.titles:
-            self.cmb_titles.addItem(title)
-        self.cmb_titles.currentIndexChanged.connect(
-            self.cmb_titles_slot)
         self.btn_recommend.clicked.connect(self.btn_recommend_slot)
 
+    #검색버튼함수
     def btn_recommend_slot(self):
-        sentence = self.le_keyword.text()
+        sentence = self.te_keyword.toPlainText()
+        print(sentence)
         input_words = sentence.split()
         if len(input_words) >= 10:
             sentence_vec = self.Tfidf.transform([sentence])
-            cosine_sim = linear_kernel(sentence_vec, self.Tfidf_matrix)
-
             recommendation_titles = self.recommend_by_sentence(sentence)
-            self.lbl_recommend.setText(recommendation_titles)
-
+            self._webview.load(QUrl('http://localhost:63342/Unsupervised_NLP_for_place_recommend2/html/search_place_result.html?_ijt=95tk70fdqitcn4f7rq1o6ufcks&_ij_reload=RELOAD_ON_SAVE'))
+            self.te_keyword.hide()
+            self.btn_recommend.hide()
         else:
-
             key_word = input_words[0]
             embedding_model = Word2Vec.load('./models/word2vecModel.model')
             try:
                 sim_word = embedding_model.wv.most_similar(key_word, topn=10)
             except:
-                self.lbl_recommend.setText('제가 모르는 단어에요 ㅠㅠ')
+                self._webview.load(QUrl('http://localhost:63342/Unsupervised_NLP_for_place_recommend2/html/empty_place_result.html?_ijt=95tk70fdqitcn4f7rq1o6ufcks&_ij_reload=RELOAD_ON_SAVE'))
+                self.te_keyword.show()
+                self.btn_recommend.show()
                 return
             sentence = [key_word] * 11
             words = []
@@ -59,39 +70,31 @@ class Exam(QWidget, form_window):
 
             sentence = ' '.join(sentence)
             recommendation_titles = self.recommend_by_sentence(sentence)
-            self.lbl_recommend.setText(recommendation_titles)
+            print('debug01')
+            self._webview.load(QUrl('http://localhost:63342/Unsupervised_NLP_for_place_recommend2/html/search_place_result.html?_ijt=95tk70fdqitcn4f7rq1o6ufcks&_ij_reload=RELOAD_ON_SAVE'))
+            self.te_keyword.hide()
+            self.btn_recommend.hide()
 
-    def cmb_titles_slot(self):
-        title = self.cmb_titles.currentText()
-        recommendation_titles = self.recommend_by_movie_title(title)
-        self.lbl_recommend.setText(recommendation_titles)
+
 
     def recommend_by_sentence(self, sentence):
         sentence_vec = self.Tfidf.transform([sentence])
         cosin_sim = linear_kernel(sentence_vec,
                                   self.Tfidf_matrix)
         recommendation_titles = self.getRecommendation(cosin_sim)
-        recommendation_titles = '\n'.join(list(recommendation_titles))
-        return recommendation_titles
-
-
-    def recommend_by_movie_title(self, title):
-        movie_idx = self.df_reviews[self.df_reviews['title']==title].index[0]
-        cosine_sim = linear_kernel(self.Tfidf_matrix[movie_idx],
-                                   self.Tfidf_matrix)
-        recommendation_titles = self.getRecommendation(cosine_sim)
-        recommendation_titles = '\n'.join(list(recommendation_titles))
-        return recommendation_titles
-
+        # recommendation_titles = '\n'.join(list(recommendation_titles))
+        # return recommendation_titles
+        recommendation_titles.to_json('./output/recommendation.json') #제이슨파일 생성은 이 위치에서?
 
     def getRecommendation(self, cosine_sim):
         simScore = list(enumerate(cosine_sim[-1]))
         simScore = sorted(simScore, key=lambda x: x[1],
-                          reverse=True)
+                              reverse=True)
         simScore = simScore[1:11]
-        movieidx = [i[0] for i in simScore]
-        recMovieList = self.df_reviews.iloc[movieidx]
-        return recMovieList.iloc[:, 0]
+        touridx = [i[0] for i in simScore]
+        recTourList = self.df_contents.iloc[touridx, :]
+        return recTourList
+
 
 
 app = QApplication(sys.argv)
