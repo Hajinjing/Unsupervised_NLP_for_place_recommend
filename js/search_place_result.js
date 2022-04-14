edbcd783830fd381bfb5a7ba19052a22cfe46af0
gangtaewon <rkdxodnjs94@naver.com>
2022-04-14 11:40:36 +0900

    더보기 업데이트

--- js/search_place_result.js
+++ js/search_place_result.js
@@ -1,10 +1,10 @@
 $.get('../output/recommendation.json')
 .done((data) => {  
-    var key_list = Object.keys(data.title)
+    var key_list = Object.keys(data.title);
     // console.log(key_list);
     for (var i in key_list) {
         var template = `
-        <div class="row col-md-6 mt-2 mb-2 items" data-id=`+ i +`>
+        <div class="row col-md-6 mt-2 mb-2 items">
           <div class="col-md-6 flex-shrink-0">
             <h5>`+ data.title[key_list[i]] +`</h5>
             <img src="../image/parkimg.jpeg" alt="...">
@@ -32,18 +32,20 @@ $.get('../output/recommendation.json')
 .fail(function(){
   console.log('실패함');
 })
-console.log($('.items').data('id',4).length);
+
+let idx = 4;
 $('.plus').on('click',function(){
-  for (var j=4; j<10; j++){
-    if (($('.items').data('id',j).length) % 4 == 0 ) {
-      $('.items').data('id',j).fadeIn();
-      $('.items').data('id',j).removeAttr('style');
-      $('.items').data('id',j).removeClass('items-hide');
-    }else {
-      $('.items').data('id',j).fadeIn();
-      $('.items').data('id',j).removeAttr('style');
-      $('.items').data('id',j).removeClass('items-hide');
-      $('.plus').addClass('items-hide');
-    } 
+  console.log(idx);
+  if (idx == 10){
+    $('.plus').fadeIn();
+    $('.plus').removeAttr('style');
+    $('.plus').hide();
   }
+  
+  for (var j=idx; j<(idx+2); j++){
+    $('.items').eq(j).fadeIn();
+    $('.items').eq(j).removeAttr('style');
+    $('.items').eq(j).removeClass('items-hide');
+  }
+  idx += 2;
 })
